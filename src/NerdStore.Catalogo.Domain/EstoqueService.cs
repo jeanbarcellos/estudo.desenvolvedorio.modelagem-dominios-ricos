b8dbc737bebdf798791fd10ba73da355fd8529a6
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-27 14:49:32 -0300

    07-08 - Lançando eventos de domínio

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,4 +1,6 @@
-﻿using NerdStore.Catalogo.Domain.Services;
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Catalogo.Domain.Services;
+using NerdStore.Core.Bus;
 using System;
 using System.Threading.Tasks;
 
@@ -7,10 +9,12 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
@@ -22,6 +26,12 @@ namespace NerdStore.Catalogo.Domain
             if (!produto.PossuiEstoque(quantidade)) return false;
 
             produto.DebitarEstoque(quantidade);
+
+            // TODO: Parametrizar a quantidade de estoque baixa
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
 
---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
+
+            // Enviar um e-mail para aquisicao de mais produtos.
+        }
+    }
+}
