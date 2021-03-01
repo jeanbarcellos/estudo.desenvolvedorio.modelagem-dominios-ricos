ce91fcda348c199dcfd155daa3c7c4ee7787740f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-28 21:09:35 -0300

    NerdStore.Vendas.Data - Implementação da da camada de dados do contexto Vendas

---
+++ src/NerdStore.Vendas.Data/Mappings/PedidoMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Vendas.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NerdStore.Vendas.Data.Mappings
+{
+    public class PedidoMapping : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.ToTable("Pedidos");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Codigo)
+                .HasDefaultValueSql("NEXT VALUE FOR MinhaSequencia");
+
+            builder.HasMany(c => c.PedidoItens)
+                .WithOne(c => c.Pedido)
+                .HasForeignKey(c => c.PedidoId);
+        }
+    }
+}
---
+++ src/NerdStore.Vendas.Data/VendasContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using NerdStore.Core.Data;
+using System.Threading.Tasks;
+using NerdStore.Core.Messages;
+using Microsoft.EntityFrameworkCore;
+using NerdStore.Vendas.Domain;
+
+namespace NerdStore.Vendas.Data.Context
+{
+    public class VendasContext : DbContext, IUnitOfWork
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
+
+        public VendasContext(DbContextOptions<VendasContext> options)
+            : base(options)
+        {
+
+        }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                x => x.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            // Gerar um código de pedido sequencial
+            modelBuilder.HasSequence<int>("MinhaSequencia").StartsAt(1000).IncrementsBy(1);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
