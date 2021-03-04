fdf878969851bdabc40581303ef464128b327a22
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-03 22:37:47 -0300

    09-09 - Integração de BC's - Processamento do pedido

--- src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -1,4 +1,7 @@
 ﻿using MediatR;
+using NerdStore.Catalogo.Domain.Services;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,10 +10,14 @@ namespace NerdStore.Catalogo.Domain.Events
     public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IEstoqueService _estoqueService;
+        private readonly IMediatorHandler _mediatorHandler;
 
-        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        public ProdutoEventHandler(IProdutoRepository produtoRepository, IEstoqueService estoqueService, IMediatorHandler mediatorHandler)
         {
             _produtoRepository = produtoRepository;
+            _estoqueService = estoqueService;
+            _mediatorHandler = mediatorHandler;
         }
 
         public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
@@ -18,6 +25,20 @@ namespace NerdStore.Catalogo.Domain.Events
             var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
 
             // Enviar um e-mail para aquisicao de mais produtos.
+        }
+
+        public async Task Handle(PedidoIniciadoEvent mensagem, CancellationToken cancellationToken)
+        {
+            var resultado = await _estoqueService.DebitarListaProdutosPedido(mensagem.ProdutosPedido);
+
+            if (resultado)
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueConfirmadoEvent(mensagem.PedidoId, mensagem.ClienteId, mensagem.Total, mensagem.ProdutosPedido, mensagem.NomeCartao, mensagem.NumeroCartao, mensagem.ExpiracaoCartao, mensagem.CVVCartao));
+            }
+            else
+            {
+                await _mediatorHandler.PublicarEvento(new PedidoEstoqueRejeitadoEvent(mensagem.PedidoId, mensagem.ClienteId));
+            }
         }
     }
 }
--- src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -7,6 +7,7 @@ using NerdStore.Core.Communication.Mediator;
 using MediatR;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Queries;
+using NerdStore.Vendas.Application.Queries.ViewModels;
 
 namespace NerdStore.WebApp.MVC.Controllers
 {
@@ -111,5 +112,30 @@ namespace NerdStore.WebApp.MVC.Controllers
             return View(nameof(Index), await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
         }
 
+        [HttpGet]
+        [Route("resumo-da-compra")]
+        public async Task<IActionResult> ResumoDaCompra()
+        {
+            return View(await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
+
+        [HttpPost]
+        [Route("iniciar-pedido")]
+        public async Task<IActionResult> IniciarPedido(CarrinhoViewModel carrinhoViewModel)
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+
+            var command = new IniciarPedidoCommand(carrinho.PedidoId, ClienteId, carrinho.ValorTotal,
+                carrinhoViewModel.Pagamento.NomeCartao, carrinhoViewModel.Pagamento.NumeroCartao,
+                carrinhoViewModel.Pagamento.ExpiracaoCartao, carrinhoViewModel.Pagamento.CVVCartao
+            );
+
+            await _mediatorHandler.EnviarComando(command);
+
+            if (OperacaoValida())
+                return RedirectToAction(nameof(Index), "Pedido");
+
+            return View("ResumoDaCompra", await _pedidoQueries.ObterCarrinhoCliente(ClienteId));
+        }
     }
 }
