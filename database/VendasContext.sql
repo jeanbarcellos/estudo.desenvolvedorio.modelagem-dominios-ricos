d6059cafb94442c50952dc8f89def5e1ba3eca6d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-29 15:29:03 -0300

    database - comandos SQL em VendasContext.sql

--- database/VendasContext.sql
+++ database/VendasContext.sql
@@ -1,3 +1,6 @@
+
+
+
 -- ----------------------------
 -- Table structure for PedidoItens
 -- ----------------------------
@@ -100,3 +103,14 @@ ALTER TABLE "public"."PedidoItens" ADD CONSTRAINT "FK_PedidoItens_Pedidos_Pedido
 -- Foreign Keys structure for table Pedidos
 -- ----------------------------
 ALTER TABLE "public"."Pedidos" ADD CONSTRAINT "FK_Pedidos_Vouchers_VoucherId" FOREIGN KEY ("VoucherId") REFERENCES "public"."Vouchers" ("Id") ON DELETE RESTRICT ON UPDATE NO ACTION;
+
+
+
+
+
+
+
+
+
+INSERT INTO "public"."Vouchers"("Id", "Codigo", "Percentual", "ValorDesconto", "Quantidade", "TipoDescontoVoucher", "DataCriacao", "DataUtilizacao", "DataValidade", "Ativo", "Utilizado") VALUES ('3ccbaf0b-9793-42cf-a27c-6f51c93e95d6', 'PROMO-15-REAIS', NULL, '15.00', 50, 1, '2021-02-01 00:00:00', NULL, '2021-12-31 00:00:00', 't', 'f');
+INSERT INTO "public"."Vouchers"("Id", "Codigo", "Percentual", "ValorDesconto", "Quantidade", "TipoDescontoVoucher", "DataCriacao", "DataUtilizacao", "DataValidade", "Ativo", "Utilizado") VALUES ('20bc1b93-5111-4d32-ab3e-779222bbdff9', 'PROMO-10-OFF', '10.00', NULL, 50, 0, '2021-02-01 00:00:00', NULL, '2021-12-31 00:00:00', 't', 'f');
